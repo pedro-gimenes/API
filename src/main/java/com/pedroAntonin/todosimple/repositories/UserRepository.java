e54044c1ed7097222b10b54a29aeea46bb2a843c
pedro-gimenes <pdrgimenes@gmail.com]>
2024-10-04 11:35:33 -0300

    Create UserService

---
+++ src/main/java/com/pedroAntonin/todosimple/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.pedroAntonin.todosimple.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pedroAntonin.todosimple.Models.User;
+
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    
+}
