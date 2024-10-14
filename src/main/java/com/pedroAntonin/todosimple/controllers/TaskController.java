995b4b2d716a919c05770695d92554de95b0233e
pedro-gimenes <pdrgimenes@gmail.com]>
2024-10-14 13:12:00 -0300

    Create view

--- src/main/java/com/pedroAntonin/todosimple/controllers/TaskController.java
+++ src/main/java/com/pedroAntonin/todosimple/controllers/TaskController.java
@@ -41,7 +41,7 @@ public class TaskController {
 
     @GetMapping("/user/{userId}")
     public ResponseEntity<List<Task>> findAllByUserId(@PathVariable Long userId) {
-        userService.findById(userId);
+        this.userService.findById(userId);
         List<Task> objs = this.taskService.findAllByUserId(userId);
         return ResponseEntity.ok().body(objs);
     }
